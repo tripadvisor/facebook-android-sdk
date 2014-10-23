4584500c194988fc2e4edb87c44b2d21c05b8372
rshah <rshah@tripadvisor.com>
2014-10-23 10:46:03 -0700

    removing unused import

--- facebook/src/com/facebook/widget/LoginButton.java
+++ facebook/src/com/facebook/widget/LoginButton.java
@@ -28,7 +28,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.AttributeSet;
-import android.util.DebugUtils;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
